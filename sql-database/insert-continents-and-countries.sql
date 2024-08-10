e6a4a2c204d71aa78c0aa882d47054653a025ba9
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-08-10 09:49:05 +0300

    Add files via upload

---
+++ sql-database/insert-continents-and-countries.sql
@@ -0,0 +1,21 @@
+INSERT INTO Continents ([Name]) VALUES
+('Europe'),
+('Europe/Asia'),
+('Asia');
+
+INSERT INTO Countries([Name], ContinentId) VALUES
+('Armenia', 2),
+('Azerbaijan', 2),
+('Belarus', 1),
+('Estonia', 1),
+('Georgia', 2),
+('Kazakhstan', 3),
+('Kyrgyzstan', 3),
+('Latvia', 1),
+('Lithuania', 1),
+('Russia', 2),
+('Tajikistan', 3),
+('Turkmenistan', 3),
+('Ukraine', 1),
+('Uzbekistan', 3);
+
